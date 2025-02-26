2ad9394e76b16433b8119e902d341a3203904250
Illia_Gorobets <gorobecillya2007@gmail.com>
2025-02-26 21:36:40 +0200

    Додав скріпти на рух двох персонажів

---
+++ Assets/Scripts/MovingP1.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class MovingP1 : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float move = 0;
+
+        if (Input.GetKey(KeyCode.A)) move = -1;
+        if (Input.GetKey(KeyCode.D)) move = 1;
+
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+}
